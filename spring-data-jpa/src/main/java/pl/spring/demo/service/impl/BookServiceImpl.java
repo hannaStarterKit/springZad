ebd41b8df99fa6094681682b282fec91d501c7d1
hannaStarterKit <hnsienkiewicz@gmail.com>
2016-05-04 21:27:44 +0200

    init

---
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
@@ -0,0 +1,44 @@
+package pl.spring.demo.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import pl.spring.demo.dao.BookDao;
+import pl.spring.demo.service.BookService;
+import pl.spring.demo.to.BookTo;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+	
+
+    private BookDao bookDao;
+
+    @Override
+    @Cacheable("booksCache")
+    public List<BookTo> findAllBooks() {
+        return bookDao.findAll();
+    }
+
+    @Override
+    public List<BookTo> findBooksByTitle(String title) {
+        return bookDao.findBookByTitle(title);
+    }
+
+    @Override
+    public List<BookTo> findBooksByAuthor(String author) {
+        return bookDao.findBooksByAuthor(author);
+    }
+
+    @Override
+    public BookTo saveBook(BookTo book) {
+        return bookDao.save(book);
+    }
+    
+    @Autowired
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+}
